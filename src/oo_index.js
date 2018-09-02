9c5bbefb804f6a8c129c4f38de843bbb34a68c99
github username <github email address>
2018-09-02 13:51:40 -0400

    everything so far

--- src/oo_index.js
+++ src/oo_index.js
@@ -1,4 +1,14 @@
 
 document.addEventListener("DOMContentLoaded", () => {
   const taskList = new TaskList();
+  const form = document.getElementById('create-task-form')
+
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    let input = document.getElementById('new-task-description');
+    const task = new Task(input.value);
+    let tasks = document.getElementById("tasks")
+    console.log(task)
+    tasks.append(task.myLi(task.description))
+  }  )
 });
--- src/task.js
+++ src/task.js
@@ -1,2 +1,16 @@
 class Task {
+  constructor(description){
+    this.description = description
+  }
+
+  myLi(task){
+    const li = document.createElement("li")
+    li.innerText = task
+    li.id = task.id
+    const delete_button = document.createElement('button')
+    delete_button.innerText = "X"
+    delete_button.addEventListener('click', console.log(this))
+    li.append(delete_button)
+    return li
+  }
 }
