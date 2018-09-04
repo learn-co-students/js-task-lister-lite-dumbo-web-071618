c0c4fa0093b1de866ffbf3b94da1d01dd34f4475
github username <github email address>
2018-09-04 18:43:31 -0400

    finished delivrables and first stretch goal

--- src/task.js
+++ src/task.js
@@ -1,2 +1,21 @@
 class Task {
+  constructor(content){
+    this.content = content
+  }
+
+  showNewTask(task) {
+    const listOfTasks = document.getElementById('tasks')
+    const taskContent = document.createElement('p')
+    taskContent.innerHTML = task.content
+
+    const deleteButton = document.createElement('button')
+    deleteButton.innerHTML = 'X'
+
+    deleteButton.addEventListener('click', e => {
+      taskContent.innerHTML = ''
+    })
+
+    listOfTasks.append(taskContent)
+    taskContent.append(deleteButton)
+  }
 }
--- src/taskList.js
+++ src/taskList.js
@@ -1,2 +1,19 @@
 class TaskList {
+  constructor(task){
+    this.task = task
+  }
+
+  receiveInput() {
+    const newForm = document.getElementById('create-task-form')
+    newForm.addEventListener('submit', function (e) {
+
+    e.preventDefault()
+
+    const newInput = document.getElementById('new-task-description')
+    const newTask = new Task(newInput.value)
+
+    newForm.reset()
+    newTask.showNewTask(newTask)
+    })
+  }
 }
